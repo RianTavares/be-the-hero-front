6d7557e5a9d6fa73126606717f7aaeb6655e27d1
riantavares <rian.tavares@live.com>
2020-03-28 20:11:23 -0300

    integrando a página de login e redirecionando user pro seu profile

--- client/src/pages/Logon/index.js
+++ client/src/pages/Logon/index.js
@@ -1,19 +1,42 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './styles.css'
 import heroesImage from '../../assets/heroes.png'
 import logoImage from '../../assets/logo.svg'
 import { FiLogIn } from 'react-icons/fi'
-import { Link } from 'react-router-dom'
+import { Link, useHistory } from 'react-router-dom'
+import api from '../../services/api'
 
 export default function Logon() {
+    const [id, setId] = useState('')
+
+    const history = useHistory()
+
+    const handleLogin = async (event) => {
+        event.preventDefault()
+
+        try {
+            const response = await api.post('sessions', { id })
+            localStorage.setItem('ongId', id)
+            localStorage.setItem('ongName', response.data.name)
+            history.push('/profile')
+        } catch (error) {
+            alert('Falha no login, tente novamente')
+        }
+        
+    }
+
     return (
         <div className="logon-container">
             <section className="form">
                 <img src={logoImage} alt="be the hero" />
-                <form>
+                <form onSubmit={handleLogin}>
                     <h1>Faça seu logon</h1>
 
-                    <input placeholder="Sua ID" />
+                    <input 
+                        placeholder="Sua ID" 
+                        value={id}
+                        onChange={e => setId(e.target.value)}
+                    />
                     <button className="button" type="submit">Entrar</button>
                     <Link className="back-link" to="/register">
                         <FiLogIn size={16} color="E02041"/>
