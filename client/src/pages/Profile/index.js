4d7747bc8ad24a95d95fd759a5b3397686e128df
riantavares <rian.tavares@live.com>
2020-03-28 20:37:57 -0300

    integrando a pagina de casos para lista e deletar casos

--- client/src/pages/Profile/index.js
+++ client/src/pages/Profile/index.js
@@ -1,16 +1,46 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './styles.css'
 import logoImage from '../../assets/logo.svg'
 import { FiPower } from 'react-icons/fi'
 import { FiTrash2 } from 'react-icons/fi'
 import { Link } from 'react-router-dom'
+import api from '../../services/api'
 
 export default function Logon() {
+    const [incidents, setIncidents] = useState([])
+    const ongName = localStorage.getItem('ongName')
+    const ongId = localStorage.getItem('ongId')
+
+    async function handleDeletIncident(id) {
+
+        try {
+            await api.delete(`incidents/${id}`, {
+                headers: {
+                    Authorization: ongId,
+                }
+            })
+
+            setIncidents(incidents.filter(incident => incident.id != id))
+        } catch (error) {
+            alert('Erro ao deletar caso, tente novamente')
+        }
+
+    }
+
+    useEffect(() => {
+        api.get('profile', {
+            headers: {
+                Authorization: ongId,
+            }
+        }).then(response => {
+            setIncidents(response.data)
+        })
+    }, [ongId])
     return (
         <div className="profile-container">
             <header>
               <img src={logoImage} alt="be the hero"/>  
-              <span>Bem vinda, APAD</span>
+              <span>Bem vinda, {ongName}</span>
             
                 <Link to="/incidents/new" className="button">Cadastrar novo caso</Link>
                 <button type="button">
@@ -21,62 +51,22 @@ export default function Logon() {
             <h1>Casos Cadastrados</h1>
 
             <ul>
-                <li>
+                {incidents.map(incident => (
+                    <li key={incident.id}>
                     <strong>CASO:</strong>
-                    <p>Caso teste</p>
+                    <p>{incident.title}</p>
 
                     <strong>DESCRIÇÃO:</strong>
-                    <p>Descrição teste</p>
+                    <p>{incident.description}</p>
 
                     <strong>VALOR:</strong>
-                    <p>R$ 120,00</p>
+                    <p>{Intl.NumberFormat('pt-BR', {style: 'currency', currency: 'BRL'}).format(incident.value)}</p>
 
-                    <button type="button">
+                    <button type="button" onClick={() => handleDeletIncident(incident.id)}>
                         <FiTrash2 size={20} color="#a8a8b3"/>
                     </button>
                 </li>
-                <li>
-                    <strong>CASO:</strong>
-                    <p>Caso teste</p>
-
-                    <strong>DESCRIÇÃO:</strong>
-                    <p>Descrição teste</p>
-
-                    <strong>VALOR:</strong>
-                    <p>R$ 120,00</p>
-
-                    <button type="button">
-                        <FiTrash2 size={20} color="#a8a8b3"/>
-                    </button>
-                </li>
-                <li>
-                    <strong>CASO:</strong>
-                    <p>Caso teste</p>
-
-                    <strong>DESCRIÇÃO:</strong>
-                    <p>Descrição teste</p>
-
-                    <strong>VALOR:</strong>
-                    <p>R$ 120,00</p>
-
-                    <button type="button">
-                        <FiTrash2 size={20} color="#a8a8b3"/>
-                    </button>
-                </li>
-                <li>
-                    <strong>CASO:</strong>
-                    <p>Caso teste</p>
-
-                    <strong>DESCRIÇÃO:</strong>
-                    <p>Descrição teste</p>
-
-                    <strong>VALOR:</strong>
-                    <p>R$ 120,00</p>
-
-                    <button type="button">
-                        <FiTrash2 size={20} color="#a8a8b3"/>
-                    </button>
-                </li>
+                ))}
             </ul>
         </div>
     );
